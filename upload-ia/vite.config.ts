c411f1aa19be1c52ca79c9fe04430d4659384b48
RxJsvini <Vinicius.Pereira@gotobiz.com.br>
2023-09-13 18:01:31 -0300

    Publicando na vercel

--- upload-ia/vite.config.ts
+++ upload-ia/vite.config.ts
@@ -4,6 +4,10 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+
+    build:{
+        outDir: 'build'
+    },
     plugins: [react()],
     resolve:{
         alias: {
